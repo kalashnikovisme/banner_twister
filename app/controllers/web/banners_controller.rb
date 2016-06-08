b8b634cd1ffa0de80bcf45d657b843c9cf65c5d0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-06-08 22:59:24 +0400

    Load banners

--- app/controllers/web/banners_controller.rb
+++ app/controllers/web/banners_controller.rb
@@ -1,4 +1,11 @@
 class Web::BannersController < ApplicationController
   def show
+    categories = Category.where name: params[:category]
+    loop do
+      @banner = categories.shuffle!.banners.where.not(count: 0).shuffle!.first
+      break if @banner.id != session[:last_banner_id]
+    end
+    session[:last_banner_id] = @banner.id
+    @banner.update_attributes count: @banner.count - 1
   end
 end
--- bin/banners.rb
+++ bin/banners.rb
@@ -1,16 +1,11 @@
 require 'csv'
 
-module Banners
-  def load_banners(filepath)
-    csv = CSV.parse File.read filepath 
-    csv.each do |row|
-      banner = Banner.create url: row[0], count: row[1]
-      row.each_with_index do |category, index|
-        if index > 1
-          banner.categories << Category.find_or_create_by(name: category)
-        end
-      end
+csv = CSV.parse File.read ARGV[0]
+csv.each do |row|
+  banner = Banner.create url: row[0], count: row[1]
+  row.each_with_index do |category, index|
+    if index > 1
+      banner.categories << Category.find_or_create_by(name: category)
     end
   end
 end
-
