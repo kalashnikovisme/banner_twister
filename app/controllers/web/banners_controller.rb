1b6efc1db2bb97bacef5a0a17e714754d8cbbc53
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-06-08 23:09:23 +0400

    Add not watching func

--- app/controllers/web/banners_controller.rb
+++ app/controllers/web/banners_controller.rb
@@ -1,11 +1,18 @@
 class Web::BannersController < ApplicationController
   def show
+    session[:last_banner_ids] ||= []
     categories = Category.where name: params[:category]
+    index = 0
+    banners = Banner.where.not(count: 0).where(id: categories.map(&:banners).flatten.map(&:id)).sort { |a, b| b <=> a }
     loop do
-      @banner = categories.shuffle.first.banners.where.not(count: 0).shuffle.first
-      break if @banner.id != session[:last_banner_id]
+      @banner = banners[index]
+      if session[:last_banner_ids].include? @banner.id
+        index += 1
+      else
+        break 
+      end
     end
-    session[:last_banner_id] = @banner.id
+    session[:last_banner_ids] << @banner.id
     @banner.update_attributes count: @banner.count - 1
   end
 end
